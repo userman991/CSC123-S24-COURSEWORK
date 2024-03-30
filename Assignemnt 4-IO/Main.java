dd15c8caedf2793f6e29b0abf97b94b601dac0cb
userman991 <jgonzalez1071@toromail.csudh.edu>
2024-03-29 23:57:37 -0700

    MyScanner

---
+++ Assignemnt 4-IO/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) 
+	{
+
+		MyScanner keyboard= new MyScanner(System.in);
+		System.out.println("Enter Num:");
+		int num=keyboard.nextInt();
+		System.out.println("You Entered: "+num);
+
+	}
+
+}
